56e4bc66a504c90c7e2a0fa34bd3867d115eda19
Ivan Capobianco <capobianco.ivan@gmail.com>
2024-02-08 11:19:19 +0100

    repo init

---
+++ src/main.cpp
@@ -0,0 +1,95 @@
+#include "OV2640.h"
+#include <Arduino.h>
+#include <WebServer.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+#include "CRtspSession.h"
+#include "OV2640Streamer.h"
+#include "SimStreamer.h"
+#include "wifikeys.h"
+
+OV2640 cam;
+
+WebServer server(80);
+
+void handle_jpg_stream(void) {
+  WiFiClient client = server.client();
+  String response = "HTTP/1.1 200 OK\r\n";
+  response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+  server.sendContent(response);
+
+  while (1) {
+    cam.run();
+    if (!client.connected())
+      break;
+    response = "--frame\r\n";
+    response += "Content-Type: image/jpeg\r\n\r\n";
+    server.sendContent(response);
+
+    client.write((char *)cam.getfb(), cam.getSize());
+    server.sendContent("\r\n");
+    if (!client.connected())
+      break;
+  }
+}
+
+void handle_jpg(void) {
+  WiFiClient client = server.client();
+
+  cam.run();
+  if (!client.connected()) {
+    return;
+  }
+  String response = "HTTP/1.1 200 OK\r\n";
+  response += "Content-disposition: inline; filename=capture.jpg\r\n";
+  response += "Content-type: image/jpeg\r\n\r\n";
+  server.sendContent(response);
+  client.write((char *)cam.getfb(), cam.getSize());
+}
+
+void handleNotFound() {
+  String message = "Server is running!\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  server.send(200, "text/plain", message);
+}
+
+void setup() {
+  Serial.println("booting");
+
+  Serial.begin(115200);
+  while (!Serial) {
+    ;
+  }
+  cam.init(esp32cam_aithinker_config);
+
+  IPAddress ip;
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  ip = WiFi.localIP();
+  Serial.println(F("WiFi connected"));
+  Serial.println("");
+  Serial.println(ip);
+
+  server.on("/", HTTP_GET, handle_jpg_stream);
+  server.on("/jpg", HTTP_GET, handle_jpg);
+  server.onNotFound(handleNotFound);
+  server.begin();
+}
+
+CStreamer *streamer;
+CRtspSession *session;
+WiFiClient client;
+
+void loop() { server.handleClient(); }
